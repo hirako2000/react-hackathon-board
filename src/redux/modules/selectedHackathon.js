5475fb0ea191b309e2da8d011b06a99eb780cd65
meed <meed2000@gmail.com>
2016-08-12 16:38:09 +0800

    Adds selectedHackathon redux module

---
+++ src/redux/modules/selectedHackathon.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { reducer as notifReducer, actions as notifActions, Notifs } from 're-notif';
+const { notifSend } = notifActions;
+import notification from './notification';
+
+export const SELECTED = 'SELECTED';
+
+// ------------------------------------
+// Actions
+// ------------------------------------
+export function selected (value: Object): Action {
+  return {
+    type: SELECTED,
+    payload: value
+  };
+}
+
+export const select = (hackathon) => (dispatch) => {
+  dispatch(selected(hackathon));
+};
+
+export const listFromServer = () => (dispatch) => {
+  axios.get('/api/hackathons')
+    .then((res) => {
+      var response = res.data;
+      for (var i = 0; i < response.length; i++) {
+        if (response[i].active) {
+          dispatch(select(response[i]));
+          break;
+        }
+      }
+    });
+};
+
+export const actions = {
+  select,
+  listFromServer
+};
+
+// ------------------------------------
+// Action Handlers
+// ------------------------------------
+const ACTION_HANDLERS = {
+  [SELECTED]: (state: Object, action: {payload: Object}): Object => action.payload
+};
+
+// ------------------------------------
+// Reducer
+// ------------------------------------
+const initialState = new Object();
+export default function selectedHackathonReducer (state: Object = initialState, action: Action): Object {
+  const handler = ACTION_HANDLERS[action.type];
+
+  return handler ? handler(state, action) : state;
+}

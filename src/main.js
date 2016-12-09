15de19500919375fefe1a9e5ce3a2b777fd5ce1f
meed <meed2000@gmail.com>
2016-12-09 11:47:11 +0700

    Removes browser history value from URL params

--- src/main.js
+++ src/main.js
@@ -14,7 +14,8 @@ import configureStore from './redux/configureStore';
 
 // Configure history for react-router
 const browserHistory = useRouterHistory(createBrowserHistory)({
-  basename: __BASENAME__
+  basename: __BASENAME__,
+  queryKey: false
 });
 
 // Create redux store and sync with react-router-redux. We have installed the
